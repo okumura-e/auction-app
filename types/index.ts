17b2f9a0fe7b03a447b2af56beef7a1eefc47202
Adriel Itamar <adrielitamar@gmail.com>
2025-03-04 10:14:19 -0300

    feat: add types

---
+++ types/index.ts
@@ -0,0 +1,27 @@
+export interface User {
+  id: string
+  name: string
+  cpf: string
+  role: "admin" | "user"
+}
+
+export interface Auction {
+  id: string
+  name: string
+  quantity: number
+  initialPrice: number
+  currentPrice: number | null
+  startDate: string
+  endDate: string
+  createdAt: string
+}
+
+export interface Bid {
+  id: string
+  auctionId: string
+  userId: string
+  userName: string
+  amount: number
+  createdAt: string
+}
+
