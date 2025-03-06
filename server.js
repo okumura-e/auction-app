90922b04b84835f4313387b0c82e62342a3b310b
Adriel Itamar <adrielitamar@gmail.com>
2025-03-06 18:35:34 -0300

    refactor: bd and server

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
-// server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const io = require('socket.io')(3001, {
   cors: {
-    origin: "http://localhost:3002", // Permite conexões do Next.js
-    methods: ["GET", "POST"]
+    origin: "http://localhost:3002",
+    methods: ["GET", "POST", "PUT"]
   }
 });
 
-// Middlewares do json-server
 server.use(middlewares);
 server.use(router);
 
-// Iniciar o json-server na porta 3000
 server.listen(3000, () => {
   console.log('JSON Server is running on port 3000');
 });
 
-// Configurações do socket.io
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('new-bid', (bid) => {
+    console.log('New bid received:', bid);
+    io.emit('new-bid', bid);
   });
 
-  socket.on('new-post', (post) => {
-    console.log('New post received:', post);
-    io.emit('new-post', post); // Envia o post para todos os clientes
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
