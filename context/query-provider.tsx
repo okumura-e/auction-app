1dda3a3c2a8989a6c08ab3e5ddc833432b83e438
Adriel Itamar <adrielitamar@gmail.com>
2025-03-06 18:36:48 -0300

    refactor: remove comments

--- context/query-provider.tsx
+++ context/query-provider.tsx
@@ -11,7 +11,7 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 60 * 1000, // 1 minute
+            staleTime: 60 * 1000,
           },
         },
       }),
