197a62c0b59bc097dfa79991de62e3a6e4f22fa8
Adriel Itamar <adrielitamar@gmail.com>
2025-03-06 09:54:18 -0300

    refactor: types

--- types/index.ts
+++ types/index.ts
@@ -9,20 +9,19 @@ export interface Auction {
   id: string
   name: string
   quantity: number
-  initialPrice: number
-  currentPrice: number | null
-  startDate: string
-  endDate: string
+  startingValue: number
+  startDateTime: string
+  endDateTime: string
   createdAt: string
   status: "waiting" | "open" | "closed"
+  bids: Bid[]
 }
 
 export interface Bid {
   id: string
-  auctionId: string
   userId: string
   userName: string
   amount: number
-  createdAt: string
+  timestamp: string
 }
 
