b22a60aecda8a040343eb663b7353114cb54f200
Adriel Itamar <adrielitamar@gmail.com>
2025-03-06 09:54:44 -0300

    feat: add functions to consume api

--- hooks/use-auctions.ts
+++ hooks/use-auctions.ts
@@ -1,13 +1,44 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import { api } from "@/service/api"
 import type { Auction } from "@/types/index"
 
 export const useAuctions = () => {
   return useQuery<Auction[], Error>({
-    queryKey: ["auctions"], // Chave única para identificar a query
+    queryKey: ["auctions"],
     queryFn: async () => {
       const response = await api.get("/auctions")
       return response.data
+    },
+  })
+}
+
+export const useAuction = (auctionId: string) => {
+  return useQuery<Auction, Error>({
+    queryKey: ["auction", auctionId],
+    queryFn: async () => {
+      const response = await api.get(`/auctions/${auctionId}`)
+      console.log(response.data);
+      
+      return response.data
+    },
+  })
+}
+
+export const useCreateAuction = () => {
+  console.log(123);
+  return useMutation<Auction, Error, Auction>({
+    
+    mutationFn: async (newAuction: Auction) => {
+      console.log(newAuction);
+      
+      const response = await api.post("/auctions", newAuction)
+      return response.data
+    },
+    onSuccess: (data) => {
+      console.log("Leilão criado com sucesso:", data)
+    },
+    onError: (error) => {
+      console.error("Erro ao criar leilão:", error)
     },
   })
 }
